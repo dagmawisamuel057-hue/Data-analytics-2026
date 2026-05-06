afb84a4c156e5ba51267cfc3c74ddc2c5768b48b
dagmawisamuel057-hue <dagmawisamuel057@gmail.com>
2026-05-06 07:31:09 -0700

    add

---
+++ pre-week-5/Ex1B_variable_examples.py
@@ -0,0 +1,71 @@
+# Exercise 1B: Variable Examples
+
+# Customer information examples
+customer_id = 101  # I used an integer because customer IDs are usually numbers.
+customer_name = "Dagmawi Mulugeta"  # I used a string because names are text.
+customer_gender = "Male"  # I used a string because gender is written as text.
+customer_date_of_birth = "1998-01-01"  # I used a string because dates can be stored in text format.
+drivers_license_number = "D1234567"  # I used a string because license numbers can contain letters and numbers.
+auto_policy_number = "AUTO-98765"  # I used a string because policy numbers can include letters and symbols.
+
+# Personal information examples
+my_name = "Dagmawi Mulugeta"
+birth_city = "Addis Ababa"
+birth_country = "Ethiopia"
+
+print(customer_id)
+print(customer_name)
+print(customer_gender)
+print(customer_date_of_birth)
+print(drivers_license_number)
+print(auto_policy_number)
+
+print(my_name)
+print(birth_city)
+print(birth_country)
+
+
+# Reserved words in Python cannot be used as variable names.
+# Examples of reserved words include:
+# False
+# None
+# True
+# and
+# as
+# assert
+# async
+# await
+# break
+# class
+# continue
+# def
+# del
+# elif
+# else
+# except
+# finally
+# for
+# from
+# global
+# if
+# import
+# in
+# is
+# lambda
+# nonlocal
+# not
+# or
+# pass
+# raise
+# return
+# try
+# while
+# with
+# yield
+
+# Five reserved word definitions:
+# if: Used to run code only when a condition is true.
+# else: Used to run a different block of code when the if condition is false.
+# for: Used to loop through a sequence like a list or range.
+# while: Used to repeat code while a condition remains true.
+# def: Used to define a function in Python.
